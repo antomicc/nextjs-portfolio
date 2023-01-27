838f04b108db2663b3b47873e1b424819a36992a
josbyte-dev <ruizm.aj@gmail.com>
2023-01-27 16:38:51 -0600

    google analitycs added

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
 import '../styles/styles.scss';
 import 'swiper/swiper.scss';
 import Layout from '../components/Layout';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout classname='ligh-theme'>
-			<Component {...pageProps} />
-		</Layout>
+		<>
+			<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-TQ12M516FM" />
+			<Script
+				id='google-analytics'
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+				gtag('config', 'G-TQ12M516FM', {
+				page_path: window.location.pathname,
+				});
+			`,
+				}}
+			/>
+			<Layout classname='ligh-theme'>
+				<Component {...pageProps} />
+			</Layout>
+		</>
 	);
 }
 
